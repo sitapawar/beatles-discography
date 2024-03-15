37cf013130993a93a3eadb925ee1f9fc974875f6
sitapawar <sita_pawar@brown.edu>
2024-03-15 15:25:08 -0400

    functional app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from './components/BakeryItem'; // Adjust the import path
+
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -11,19 +13,50 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [cart, setCart] = useState([]);
+  const [itemCounts, setItemCounts] = useState({}); // State to keep track of item counts
+  
+  const addToCart = (item) => {
+    setCart([...cart, item]);
+    setItemCounts(prevCounts => ({
+      ...prevCounts,
+      [item.name]: (prevCounts[item.name] || 0) + 1 // Increment count for the item
+    }));
+  };
+
+  const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+  const removeFromCart = (item) => {
+    setCart(cart.filter(cartItem => cartItem !== item)); // Remove the item from the cart
+    setItemCounts(prevCounts => ({
+      ...prevCounts,
+      [item.name]: (prevCounts[item.name] || 0) - 1 // Decrement count for the item
+    }));
+  };
 
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
-
+      <h1>Sita's Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+    <div class ="pageContents">
+    <div class ="bakeryItems">
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+         <BakeryItem item={item} addToCart={addToCart} /> // replace with BakeryItem component
       ))}
-
-      <div>
-        <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
       </div>
+      <div className="Cart">
+        <h2>Cart</h2>
+        <ul>
+          {Object.keys(itemCounts).map((itemName, index) => (
+            <li key={index}>{itemCounts[itemName]} x {itemName}
+            <button onClick={() => removeFromCart(bakeryData.find(item => item.name === itemName))}>
+                Remove from Cart
+              </button>
+            </li>
+          ))}
+        </ul>
+        <p>Total: ${totalPrice.toFixed(2)}</p>
+      </div>
+    </div>
+      
     </div>
   );
 }
